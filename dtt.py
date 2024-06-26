378238dfa8b80345068d4487a97f342a59b391c3
celebrater46 <tkgm45nbchk@gmail.com>
2024-06-26 18:07:03 +0900

    test to convert successfully

--- dtt.py
+++ dtt.py
@@ -1,18 +1,32 @@
 import docx
+import os
 
-def say_hello():
-    print("Hello world!!!!!!!!!")
+DIR_PATH = "docx"
+DIR_PATH_TXT = "txt"
 
-say_hello()
+def get_filenames():
+    file_names = [
+        f for f in os.listdir(DIR_PATH) if os.path.isfile(os.path.join(DIR_PATH, f))
+    ]
+    return file_names
 
-def create_txt(doc):
-    f = open("files/converted.txt", "w", encoding='utf-8')
+def create_txt(doc, file_name):
+    txt = file_name.replace(".docx", ".txt")
+    f = open(f"{DIR_PATH_TXT}/{txt}", "w", encoding='utf-8')
     for paragraph in doc.paragraphs:
         f.write(paragraph.text + "\n")
     f.close()
+    print(f"Created: {txt}")
 
-def open_docx():
-    document = docx.Document("files/test.docx")
-    create_txt(document)
+def open_docx(dir):
+    document = docx.Document(dir)
+    # create_txt(document)
+    return document
 
-open_docx()
+def docx_to_txt():
+    fs = get_filenames()
+    for f in fs:
+        doc = open_docx(f"{DIR_PATH}/{f}")
+        create_txt(doc, f)
+
+docx_to_txt()
